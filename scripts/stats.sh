85038f5b9636725e89519deb660dde3dfcffa509
PrayagRaj22 <prayagrajsingh55@protonmail.com>
2026-05-14 16:04:02 +0530

    feat(scripts): add procfs stats collection script

---
+++ scripts/stats.sh
@@ -0,0 +1,6 @@
+(date && echo -e "\n" && cat /proc/meminfo && echo -e "\n------------------------------\n") >>  ./statistics/meminfo.txt
+
+(date && echo -e "\n" && cat /proc/cpuinfo | head -30 && echo -e "\n------------------------------\n") >>  ./statistics/cpuinfo.txt
+
+
+(date && echo -e "\n" && cat /proc/net/dev && echo -e "\n------------------------------\n") >>  ./statistics/netinfo.txt
